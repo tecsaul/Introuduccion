580023ae1ef093b6fce3f2413c014519574765f5
saul nuñez <77266273+tecsaul@users.noreply.github.com>
2022-09-25 20:08:53 -0700

    Add files via upload

---
+++ Introduccion/jsScript.js
@@ -0,0 +1,20 @@
+//console.log("here im again");
+//var myfirsVar = "Saul";
+//myfirsVar = "hello";
+//var age=23;
+
+const tax = 12;
+let userName="Saul"
+let age = 23;
+
+
+
+//console.log("My name is: "+ userName + " and my age is: "+age);
+//console.log("the constant values is "+ tax);
+
+document.write("<p> Name: "+userName +"and the age is: "+age+ "</p>");
+
+document.write(`
+<p>Name: ${userName} </p>
+<p>age is: ${age}</p>
+`)
